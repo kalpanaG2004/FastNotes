23470cdcecefb670c552e9744283f44f962b00e1
Kalpana Gupta <kalpanagupta592004@gmail.com>
2025-04-21 23:54:08 +0530

    some updates

--- config/db.py
+++ config/db.py
@@ -2,15 +2,8 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Get the MongoDB URI from the environment variables
 MONGO_URI = os.getenv("MONGO_URI")
 
-# Connect to MongoDB
-if MONGO_URI:
-    conn = MongoClient(MONGO_URI)
-    print("Successfully connected to MongoDB.")
-else:
-    print("MongoDB URI not found in environment variables.")
+conn = MongoClient(MONGO_URI)
